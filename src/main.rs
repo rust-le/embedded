42d39b3ee870431b8a2b1cf55eeb56ba2c37a486
rust-le <mateusz@rustle.pl>
2023-02-22 21:35:00 +0100

    commiting old version

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,85 @@
 // std and main are not available for bare metal software
 #![no_std]
 #![no_main]
-
+// https://jonathanklimt.de/electronics/programming/embedded-rust/rust-on-stm32-2/
+use core::fmt::Write;
+use core::str::from_utf8;
 use cortex_m_rt::entry; // The runtime
 use embedded_hal::digital::v2::OutputPin; // the `set_high/low`function
-use stm32f1xx_hal::{delay::Delay, pac, prelude::*}; // STM32F1 specific functions
+use max7219_dot_matrix::{Command, MAX7219};
+use nb::block;
+
 #[allow(unused_imports)]
 use panic_halt;
-use stm32f1xx_hal::pac::GPIOA; // When a panic occurs, stop the microcontroller
+use stm32f1xx_hal::{
+    delay::Delay,
+    pac,
+    prelude::*,
+    serial::{Config, Serial},
+};
 
-// This marks the entrypoint of our application. The cortex_m_rt creates some
-// startup code before this, but we don't need to worry about this
+// STM32F1 specific functions // When a panic occurs, stop the microcontroller
 #[entry]
 fn main() -> ! {
-    // Get handles to the hardware objects. These functions can only be called
-    // once, so that the borrowchecker can ensure you don't reconfigure
-    // something by accident.
     let dp = pac::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
 
-    // GPIO pins on the STM32F1 must be driven by the APB2 peripheral clock.
-    // This must be enabled first. The HAL provides some abstractions for
-    // us: First get a handle to the RCC peripheral:
+    let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
-    // Now we have access to the RCC's registers. The GPIOC can be enabled in
-    // RCC_APB2ENR (Prog. Ref. Manual 8.3.7), therefore we must pass this
-    // register to the `split` function.
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
     let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
-    // This gives us an exclusive handle to the GPIOC peripheral. To get the
-    // handle to a single pin, we need to configure the pin first. Pin C13
-    // is usually connected to the Bluepills onboard LED.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
     let mut output_a = gpioa.pa1.into_push_pull_output(&mut gpioa.crl);
     let mut output_b = gpioa.pa2.into_push_pull_output(&mut gpioa.crl);
     let mut output_c = gpioa.pa3.into_push_pull_output(&mut gpioa.crl);
     let mut output_d = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
 
-    // Now we need a delay object. The delay is of course depending on the clock
-    // frequency of the microcontroller, so we need to fix the frequency
-    // first. The system frequency is set via the FLASH_ACR register, so we
-    // need to get a handle to the FLASH peripheral first:
-    let mut flash = dp.FLASH.constrain();
-    // Now we can set the controllers frequency to 8 MHz:
-    let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
-    // The `clocks` handle ensures that the clocks are now configured and gives
-    // the `Delay::new` function access to the configured frequency. With
-    // this information it can later calculate how many cycles it has to
-    // wait. The function also consumes the System Timer peripheral, so that no
-    // other function can access it. Otherwise the timer could be reset during a
-    // delay.
+    //let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
     let mut delay = Delay::new(cp.SYST, clocks);
+
+    let tx = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
+    let rx = gpiob.pb11;
+
+    let mut serial = Serial::usart3(
+        dp.USART3,
+        (tx, rx),
+        &mut afio.mapr,
+        Config::default().baudrate(9600.bps()),
+        clocks,
+        &mut rcc.apb1,
+    );
+    let (mut tx, mut rx) = serial.split();
+
     loop {
-        let half_step_clockwise = [[1,0,0,0],
-                                        [1,1,0,0],
-                                        [0,1,0,0],
-                                        [0,1,1,0],
-                                        [0,0,1,0],
-                                        [0,0,1,1],
-                                        [0,0,0,1],
-                                        [1,0,0,1]];
+        block!(tx.write(b'R')).ok();
+        let received = block!(rx.read()).unwrap();
+        delay.delay_ms(100_u16);
+        led.set_high().ok();
+        delay.delay_ms(100_u16);
+        led.set_low().ok();
+    }
+    /* let half_step_clockwise = [
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1],
+    ];*/
+    /*let mut rotate_clockwise = ||{
         for _ in 0..31 {
             half_step_clockwise.iter().for_each(|tick| {
-                led.set_high().ok();
                 match tick[0] {
-                    0 => {
-                        output_a.set_low().ok();
-                        led.set_low().ok();
-                    },
-                    _ => {
-                        output_a.set_high().ok();
-                        led.set_high().ok();
-                    },
+                    0 => output_a.set_low().ok(),
+                    _ => output_a.set_high().ok(),
                 };
                 match tick[1] {
                     0 => output_b.set_low().ok(),
@@ -86,14 +93,19 @@ fn main() -> ! {
                     0 => output_d.set_low().ok(),
                     _ => output_d.set_high().ok(),
                 };
-                delay.delay_ms(5_u16);
+                delay.delay_ms(50_u16);
             });
         }
-        led.set_low().ok();
         output_a.set_low().ok();
         output_b.set_low().ok();
         output_c.set_low().ok();
         output_d.set_low().ok();
+    };*/
+    /*loop {
+        // rotate_clockwise();
         delay.delay_ms(1000_u16);
-    }
+        led.set_high().ok();
+        delay.delay_ms(200_u16);
+        led.set_low().ok();
+    }*/
 }
