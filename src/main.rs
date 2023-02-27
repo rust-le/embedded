ad2d8aa1422c436112e86684772d3fa0e90c2c02
rust-le <mateusz@rustle.pl>
2023-02-27 15:24:55 +0100

    added usb-device and usbd-serial crates
    its working app running on stm32f103 device, capturing usb input and
    switching leds to show status - see rust-le/cargo-watch-traffic-status-indicator

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,94 @@
-// std and main are not available for bare metal software
 #![no_std]
 #![no_main]
-// https://jonathanklimt.de/electronics/programming/embedded-rust/rust-on-stm32-2/
-use core::fmt::Write;
-use core::str::from_utf8;
-use cortex_m_rt::entry; // The runtime
-use embedded_hal::digital::v2::OutputPin; // the `set_high/low`function
-use nb::block;
+extern crate panic_semihosting;
+use cortex_m::asm::delay;
+use cortex_m_rt::entry;
+use stm32f1xx_hal::usb::{Peripheral, UsbBus};
+use stm32f1xx_hal::{pac, prelude::*};
+use usb_device::prelude::*;
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-#[allow(unused_imports)]
-use panic_halt;
-use stm32f1xx_hal::{
-    delay::Delay,
-    pac,
-    prelude::*,
-};
-
-// STM32F1 specific functions // When a panic occurs, stop the microcontroller
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
-
     let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.MHz())
+        .sysclk(48.MHz())
+        .pclk1(24.MHz())
+        .freeze(&mut flash.acr);
 
-    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    assert!(clocks.usbclk_valid());
 
+    let mut gpioc = dp.GPIOC.split();
+    let mut gpiob = dp.GPIOB.split();
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut green_led = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+    let mut yellow_led = gpiob.pb13.into_push_pull_output(&mut gpiob.crh);
+    let mut red_led = gpiob.pb14.into_push_pull_output(&mut gpiob.crh);
+    led.set_high(); // Turn off
+    green_led.set_low();
+    yellow_led.set_low();
+    red_led.set_low();
+    let mut gpioa = dp.GPIOA.split();
 
-    //let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut delay = Delay::new(cp.SYST, clocks);
-
-
-
+    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+    usb_dp.set_low();
+    delay(clocks.sysclk().raw() / 100);
+    let usb = Peripheral {
+        usb: dp.USB,
+        pin_dm: gpioa.pa11,
+        pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
+    };
+    let usb_bus = UsbBus::new(usb);
+    let mut serial = SerialPort::new(&usb_bus);
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(USB_CLASS_CDC)
+        .build();
     loop {
-        delay.delay_ms(100_u16);
-        led.set_high().ok();
-        delay.delay_ms(100_u16);
-        led.set_low().ok();
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+        let mut buf = [0u8; 8];
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                led.set_low();
+                for c in buf[0..count].iter_mut() {
+                    if *c >= 0x30 && *c <= 0x37 {
+                        match *c & (1 << 0) {
+                            0 => {
+                                red_led.set_low();
+                            }
+                            _ => {
+                                red_led.set_high();
+                            }
+                        }
+                        match *c & (1 << 1) {
+                            0 => {
+                                yellow_led.set_low();
+                            }
+                            _ => {
+                                yellow_led.set_high();
+                            }
+                        }
+                        match *c & (1 << 2) {
+                            0 => {
+                                green_led.set_low();
+                            }
+                            _ => {
+                                green_led.set_high();
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+        led.set_high();
     }
 }
